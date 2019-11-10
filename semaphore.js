35b28db905591e0679da416503daf828363bf17f
hitallocavas <hcs3@cin.ufpe.br>
2019-11-10 10:34:19 -0300

    send body color

--- semaphore.js
+++ semaphore.js
@@ -2,27 +2,39 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const mqtt = require('mqtt')
+var bodyParser = require('body-parser')
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
 const client  = mqtt.connect('mqtt://broker.mqttdashboard.com')
 
 
   app.post('/fst', function (req, res) {
+    var color = req.body.color;
     client.subscribe('wot-semaphore/fst/led', function (err) {
-      client.publish('wot-semaphore/fst/led', req.color)
+      console.log(color);
+      client.publish('wot-semaphore/fst/led', color)
     })
     res.sendStatus(200);
   });
 
   
   app.post('/snd', function (req, res) {
+    var color = req.body.color;
     client.subscribe('wot-semaphore/snd/led', function (err) {
-      client.publish('wot-semaphore/snd/led', req.color)
+      console.log(color);
+      client.publish('wot-semaphore/snd/led', color)
     })
     res.sendStatus(200);
   });
 
-
   client.on('message', function (topic, message) {
-    console.log(topic.toString() + ": " + message.toString())
+    console.log(topic.toString() + ": " + message)
   })
 
 app.use(function(req, res, next) {
