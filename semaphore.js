97cf62a24de7d60e094474186865f7a0c881a438
arthurfaraujo <afa4@cin.ufpe.br>
2019-11-10 11:57:01 -0300

    Envio de informações dos sensores-sonar via web-sockets

--- semaphore.js
+++ semaphore.js
@@ -1,54 +1,63 @@
+// Config
 const express = require('express')
 const path = require('path')
 const app = express()
 const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://broker.mqttdashboard.com')
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 8080 })
 var bodyParser = require('body-parser')
-var sleep = require('system-sleep');
+var sleep = require('system-sleep')
 
+// Logic
+function semaphoreBehavior() {
+  while(true){
+    client.publish('wot-semaphore/snd/led', 'G')
+    sleep(3000);
+    client.publish('wot-semaphore/snd/led', 'Y')
+    sleep(1000);
+    client.publish('wot-semaphore/snd/led', 'R')
+    sleep(3000);
+  }
+}
 
-// configure the app to use bodyParser()
+// Comunication
+client.on('connect', function(topic,message){
+  console.log('MQTT connected');
+  client.subscribe('wot-semaphore/fst/sonar')
+  client.subscribe('wot-semaphore/snd/sonar')
+  semaphoreBehavior()
+})
+
+wss.on('connection', function(ws) {
+    console.log('WS connected')  
+    client.on('message', function (topic, message) {
+      const obj = new Object()
+      obj['topic'] = topic
+      obj['message'] = message
+      ws.send(JSON.stringifyonj)
+    })
+})
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
+app.post('/fst', function (req, res) {
+  var color = req.body.color;
+  console.log(color);
+  client.publish('wot-semaphore/fst/led', color)
+  res.sendStatus(200);
+});
 
-const client  = mqtt.connect('mqtt://broker.mqttdashboard.com')
+app.post('/snd', function (req, res) {
+  var color = req.body.color
+  console.log(color);
+  client.publish('wot-semaphore/snd/led', color)
+  res.sendStatus(200);
+});
 
-
-  app.post('/fst', function (req, res) {
-    var color = req.body.color;
-    client.subscribe('wot-semaphore/fst/led', function (err) {
-      console.log(color);
-      client.publish('wot-semaphore/fst/led', color)
-    })
-    res.sendStatus(200);
-  });
-
-  
-  app.post('/snd', function (req, res) {
-    var color = req.body.color;
-    client.subscribe('wot-semaphore/snd/led', function (err) {
-      console.log(color);
-      client.publish('wot-semaphore/snd/led', color)
-    })
-    res.sendStatus(200);
-  });
-
-  client.on('message', function (topic, message) {
-    console.log(topic.toString() + ": " + message)
-  })
-
-  client.on('connect', function(topic,message){
-    while(true){
-      client.publish('wot-semaphore/snd/led', 'R')
-      sleep(3000);
-      client.publish('wot-semaphore/snd/led', 'Y')
-      sleep(2000);
-      client.publish('wot-semaphore/snd/led', 'G')
-      sleep(3000);
-    }
-  })
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
